384d71581c680fe2d745202cf626514df629ba39
容玉钏 [7652289183771692275] <>
2026-07-02 13:36:34 +0800

    添加统计页考核详情路由statistics/assessme

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -87,6 +87,14 @@ const RoutesComponent = () => {
               }
             />
             <Route
+              path="statistics/assessment/:id"
+              element={
+                <ProtectedRoute roles={MANAGER_ROLES}>
+                  <AssessmentDetailPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
               path="my-assessments"
               element={
                 <ProtectedRoute roles={ALL_ROLES}>
